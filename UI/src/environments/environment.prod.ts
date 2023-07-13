fbece6193d45c27655bb12ce1f11b5df3608baef
alondhe <londhe@ohdsi.org>
2023-07-12 23:41:38 -0700

    Trying serverUrl approach in environment prod file

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { AuthStrategies } from './auth-strategies'
 
 export const environment = {
   production: true,
-  serverUrl: null,
+  serverUrl: window["env"]["serverUrl"] || null,
   conceptTables: CONCEPT_TABLES,
   authStrategy: AuthStrategies.SMTP
 };
